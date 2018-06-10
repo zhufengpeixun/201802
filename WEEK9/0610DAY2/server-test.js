0fbce25acb66c75462454542235d412ab89d6df2
zhouxiaotian <1282347298@qq.com>
2018-06-10 18:56:30 +0800

    NODE

--- WEEK9/0610DAY2/server-test.js
+++ WEEK9/0610DAY2/server-test.js
@@ -1,6 +1,12 @@
-let {readFile} = require('./utils/fsPromise');
-readFile(`./static/img/title.png`).then(result => {
-    console.log(result);
-}).catch(error => {
-    console.log(error);
-});
+// let {readFile} = require('./utils/fsPromise');
+// readFile(`./static/img/title.png`).then(result => {
+//     console.log(result);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+// let url=require('url');
+// console.log(url.parse('/temp?name=xxx&age=25', true));
+
+// let qs = require('qs');
+// console.log(qs.parse('name=xxx&age=25'));
--- WEEK9/0610DAY2/server.js
+++ WEEK9/0610DAY2/server.js
@@ -2,8 +2,29 @@ let http = require('http'),
     url = require('url'),
     path = require('path'),
     fs = require('fs');
-let {readFile} = require('./utils/fsPromise'),
-    mime = require('mime');
+let {readFile, writeFile} = require('./utils/fsPromise'),
+    mime = require('mime'),
+    qs = require('qs');
+
+//=>公共方法
+let responseResult = function responseResult(res, returnVal) {
+    res.writeHead(200, {
+        'content-type': 'application/json;charset=utf-8;'
+    });
+    res.end(JSON.stringify(returnVal));
+};
+
+let readUSER = function readUSER() {
+    return readFile(`./json/USER.JSON`).then(result => {
+        return JSON.parse(result);
+    });
+};
+
+let readVOTE = function readVOTE() {
+    return readFile(`./json/VOTE.JSON`).then(result => {
+        return JSON.parse(result);
+    });
+};
 
 //=>创建WEB服务
 let port = 8686;
@@ -31,6 +52,84 @@ let handle = function handle(req, res) {
     }
 
     //=>API接口请求处理
+
+    //=>GET-USER:根据传递的用户ID获取指定用户的信息
+    if (pathname === '/getUser' && method === 'GET') {
+        //=>问号传递的信息都在QUERY中存储着
+        let {userId = 0} = query,
+            returnVal = {code: 1, message: 'no!', data: null};
+        readUSER().then(result => {
+            let data = result.find(item => parseFloat(item['id']) === parseFloat(userId));
+            if (data) {
+                returnVal = {code: 0, message: 'ok', data};
+                responseResult(res, returnVal);
+                return;
+            }
+            throw new Error('');//=>目的是没有数据的时候,让其执行CATCH中的操作,这样我们只需要让THEN方法中有异常信息即可
+        }).catch(error => responseResult(res, returnVal));
+
+        /*readUSER().then(result => {
+            let data = result.find(item => parseFloat(item['id']) === parseFloat(userId));
+            data ? returnVal = {code: 0, message: 'ok', data} : null;
+        }).finally(result => {
+            //=>暂时NODE版本不支持FINALLY
+            responseResult(res, returnVal);
+        });*/
+        return;
+    }
+
+    //=>REGIDTER:注册用户
+    if (pathname === '/register' && method === 'POST') {
+        //=>接收客户端请求主体传递的内容
+        let pass = ``;
+        req.on('data', chunk => {
+            //=>正在接收请求主体内容,可能会被触发执行很多次,chunk获取的都是本次接收的BUFFER格式的数据
+            pass += chunk;
+        });
+        req.on('end', () => {
+            //=>已经把请求主体内容接收完成了  PASS是一个URLENCODED格式字符串，我们需要把它解析为对象
+            pass = qs.parse(pass);
+            readUSER().then(result => {
+                //=>FORMAT-PASS
+                let maxId = result.length <= 0 ? 0 : parseFloat(result[result.length - 1]['id']);
+                pass.password = pass.password.substr(4, 24).split('').reverse().join('');
+                let newData = {
+                    id: maxId + 1,
+                    name: '',
+                    picture: `img/${pass.sex != 0 ? `woman` : `man`}.png`,
+                    phone: '',
+                    sex: 0,
+                    password: '',
+                    bio: '',
+                    time: new Date().getTime(),
+                    isMatch: 0,
+                    matchId: '000',
+                    slogan: '',
+                    voteNum: 0,
+                    ...pass
+                };
+
+                //=>把NEW-DATA追加到RESULT末尾,把最新的结果重新写入到文件
+                result.push(newData);
+                return writeFile('./json/USER.JSON', result);
+            }).then(result => {
+                responseResult(res, {
+                    code: 0,
+                    message: 'ok'
+                });
+            }).catch(error => {
+                responseResult(res, {
+                    code: 1,
+                    message: 'no'
+                });
+            });
+        });
+        return;
+    }
+
+    //=>请求的都不是以上API接口,直接404即可
+    res.writeHead(404);
+    res.end('');
 };
 http.createServer(handle).listen(port, () => {
     console.log(`server is success，listen on ${port}！`);
