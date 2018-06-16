38dcd8b82c6bf98022bcb1d1dad77745698b6cbc
zhouxiaotian <1282347298@qq.com>
2018-06-16 18:35:51 +0800

    同步异步和PromiseA+

--- WEEK10/DAY1/6-promise.js
+++ WEEK10/DAY1/6-promise.js
@@ -33,11 +33,54 @@ class Promise {
     }
 
     then(fulfilledCallBack, rejectedCallBack) {
-        //=>返回一个新的PROMISE实例
-        
+        //=>处理不传递的状况
+        typeof fulfilledCallBack !== 'function' ? fulfilledCallBack = result => result : null;
+        typeof rejectedCallBack !== 'function' ? rejectedCallBack = reason => {
+            throw new Error(reason instanceof Error ? reason.message : reason);
+        } : null;
 
-        this.fulfilledAry.push(fulfilledCallBack);
-        this.rejectedAry.push(rejectedCallBack);
+        //=>返回一个新的PROMISE实例
+        return new Promise((resolve, reject) => {
+            this.fulfilledAry.push(() => {
+                try {
+                    let x = fulfilledCallBack(this.value);
+                    x instanceof Promise ? x.then(resolve, reject) : resolve(x);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+            this.rejectedAry.push(() => {
+                try {
+                    let x = rejectedCallBack(this.value);
+                    x instanceof Promise ? x.then(resolve, reject) : resolve(x);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        });
+    }
+
+    catch(rejectedCallBack) {
+        return this.then(null, rejectedCallBack);
+    }
+
+    static all(promiseAry = []) {//=>Promise.all()
+        return new Promise((resolve, reject) => {
+            //=>INDEX:记录成功的数量 RESULT:记录成功的结果
+            let index = 0,
+                result = [];
+            for (let i = 0; i < promiseAry.length; i++) {
+                //=>promiseAry[i]:
+                //每一个需要处理的PROMISE实例
+                promiseAry[i].then(val => {
+                    index++;
+                    result[i] = val;//=>索引需要和promiseAry对应上，保证结果的顺序和数组顺序一致
+                    if (index === promiseAry.length) {
+                        resolve(result);
+                    }
+                }, reject);
+            }
+        });
     }
 }
 
--- WEEK10/DAY1/6-test.js
+++ WEEK10/DAY1/6-test.js
@@ -1,22 +1,51 @@
 let Promise = require('./6-promise');
 
-let p1 = new Promise((resolve, reject) => {
+/*let p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
-        Math.random() < 0.5 ? resolve(100) : reject(-100);
+        // Math.random() < 0.5 ? resolve(100) : reject(-100);
+        resolve(100);
     }, 1000);
-
 });
 
 let p2 = p1.then(result => {
+    throw new Error('ERROR');
     return result + 100;
-}, reason => {
-    return reason + 100;
 });
 
-p2.then(result => {
-    console.log(p1 === p2);//=>FALSE 执行THEN返回的是一个新的PROMISE实例
+let p3 = p2.then(null);
+
+p3.then(result => {
     console.log(result);
 }, reason => {
+    console.log(1, reason);
+}).catch(reason => {
+    console.log(2, reason);
+});
+
+console.log(3);*/
+
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(100);
+    }, 50);
+});
+
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(200);
+    }, 10);
+});
+
+let p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(300);
+    }, 80);
+});
+
+Promise.all([p1, p2, p3]).then(result => {
+    //=>所有的PROMISE都成功执行,RESULT中分别存储每一个实例返回的结果，而且和数组中的顺序是一样的
+    console.log(result);
+}).catch(reason => {
+    //=>只要有有一个失败，就执行这个方法，失败后不再执行后面的操作
     console.log(reason);
 });
-console.log(3);
