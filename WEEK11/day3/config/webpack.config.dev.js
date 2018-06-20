965b01fc29e7eec609c0bc2b0156a5c584486e09
zhouxiaotian <1282347298@qq.com>
2018-06-20 20:19:59 +0800

    组件的状态管理

--- WEEK11/day3/config/webpack.config.dev.js
+++ WEEK11/day3/config/webpack.config.dev.js
@@ -157,7 +157,7 @@ module.exports = {
                     // In production, we use a plugin to extract that CSS to a file, but
                     // in development "style" loader enables hot editing of CSS.
                     {
-                        test: /\.css$/,
+                        test: /\.(css|less)$/,
                         use: [
                             require.resolve('style-loader'),
                             {
@@ -186,6 +186,9 @@ module.exports = {
                                     ],
                                 },
                             },
+                            {
+                                loader: require.resolve('less-loader')
+                            }
                         ],
                     },
                     // "file" loader makes sure those assets get served by WebpackDevServer.
--- WEEK11/day3/config/webpack.config.prod.js
+++ WEEK11/day3/config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
                     // use the "style" loader inside the async code so CSS from them won't be
                     // in the main CSS file.
                     {
-                        test: /\.css$/,
+                        test: /\.(css|less)$/,
                         loader: ExtractTextPlugin.extract(
                             Object.assign(
                                 {
@@ -205,6 +205,9 @@ module.exports = {
                                                 ],
                                             },
                                         },
+                                        {
+                                            loader: require.resolve('less-loader')
+                                        }
                                     ],
                                 },
                                 extractTextPluginOptions
