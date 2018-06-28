4241153be289c6e1e3686c9b81f351e7b1788ea3
zhouxiaotian <1282347298@qq.com>
2018-06-28 19:11:33 +0800

    ANTD

--- WEEK12/day3/src/routes/Home.js
+++ WEEK12/day3/src/routes/Home.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+/*ANTD*/
+//=>LocaleProvider:国际化组件，目的是把组件汉化
+import {LocaleProvider, DatePicker, Icon, Button, Calendar} from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import '../static/css/antd.css';
+
+
 class Home extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            loading: false
+        };
     }
 
     render() {
-        return <div>
-            我是首页HOME
-        </div>;
+        //=>只要LocaleProvider包含的组件都是被汉化的
+        return <LocaleProvider locale={zh_CN}>
+            <div>
+                {/*<div style={{width: '300px', margin: '10px'}}>
+                    <Calendar/>
+                </div>*/}
+
+                {/*<Icon type='zhihu' style={{
+                    fontSize: '22px',
+                    color: 'red'
+                }}/>*/}
+
+                <Button type='danger' loading={this.state.loading}
+                        onClick={ev => {
+                            this.setState({loading: true});
+                            setTimeout(() => {
+                                this.setState({loading: false});
+                            }, 3000);
+                        }}>提交</Button>
+            </div>
+        </LocaleProvider>;
     }
 }
 
