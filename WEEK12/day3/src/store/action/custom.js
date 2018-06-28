4c6cc52ac39af3b3c73b2f34d705b3fe2597f568
zhouxiaotian <1282347298@qq.com>
2018-06-28 18:20:23 +0800

    REDUX中间件

--- WEEK12/day3/src/store/action/custom.js
+++ WEEK12/day3/src/store/action/custom.js
@@ -2,10 +2,29 @@ import * as TYPES from '../action-types';
 
 let custom = {
     //=>增加客户信息:PAYLOAD={ID,NAME}
+    /*create(payload) {
+        //=>THUNK中间件的使用语法：在指定执行派发任务的时候，等待3000MS后在派发
+        return dispatch => {
+            //=>DISPATCH都传递给我们了，我们想什么时候派发，自己搞定即可
+            setTimeout(() => {
+                dispatch({
+                    type: TYPES.CUSTOM_CREATE,
+                    payload
+                });
+            }, 3000);
+        };
+    }*/
+
+    //=>PROMISE中间件的语法
     create(payload) {
         return {
             type: TYPES.CUSTOM_CREATE,
-            payload
+            //=>传递给REDUCER的PAYLOAD需要等待PROMISE成功，把成功的结果传递过去
+            payload: new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(payload);
+                }, 3000);
+            })
         }
     }
 };
--- WEEK12/day3/src/store/index.js
+++ WEEK12/day3/src/store/index.js
@@ -1,5 +1,17 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import reduxLogger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
+import reduxPromise from 'redux-promise';
 import reducer from './reducer';
 
-let store = createStore(reducer);
+let store = createStore(reducer, applyMiddleware(reduxLogger, reduxThunk, reduxPromise));
 export default store;
+
+/*
+ * REDUX中间件
+ *   redux-logger：能够在控制台清晰的展示出当前REDUX操作的流程和信息(原有状态、派发信息、修改后的状态信息)
+ *
+ *   redux-thunk：处理异步的DISPATCH派发
+ *
+ *   redux-promise：在DISPATCH派发的时候支持PROMISE操作
+ */
